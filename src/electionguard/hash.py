3364cdcf25140cc82b5b49e4551475e980e3f2fc
Jonas <jonas.kaiser@student.kit.edu>
2026-08-05 09:50:31 +0200

    Revert hash function to use no tag for previous usage of it

--- src/electionguard/hash.py
+++ src/electionguard/hash.py
@@ -5,6 +5,7 @@ from hashlib import sha256
 from typing import (
     Iterable,
     List,
+    Optional,
     Protocol,
     Union,
     runtime_checkable,
@@ -59,13 +60,17 @@ CryptoHashableAll = Union[
     CryptoHashableT,
 ]
 
-DOMAIN_TAG_DEF = b"ElectionGuard-EV/MuSig/def/v1"
-DOMAIN_TAG_AGG = b"ElectionGuard-EV/MuSig/agg/v1"
-DOMAIN_TAG_SIG = b"ElectionGuard-EV/MuSig/sig/v1"
+def make_64byte_block(tag: bytes) -> bytes:
+    hash = sha256(tag).digest()
+    return hash + hash
 
+
+DOMAIN_TAG_DEF = make_64byte_block(b"ElectionGuard/def/v1")
+DOMAIN_TAG_AGG = make_64byte_block(b"ElectionGuard/agg/v1")
+DOMAIN_TAG_SIG = make_64byte_block(b"ElectionGuard/sig/v1")
 
 def hash_elems(*a: CryptoHashableAll) -> ElementModQ:
-    return hash_elems_with_tag(DOMAIN_TAG_DEF, *a)
+    return hash_elems_with_tag(None, *a)
 
 def hash_elems_agg(*a: CryptoHashableAll) -> ElementModQ:
     return hash_elems_with_tag(DOMAIN_TAG_AGG)
@@ -73,25 +78,20 @@ def hash_elems_agg(*a: CryptoHashableAll) -> ElementModQ:
 def hash_elems_sig(*a: CryptoHashableAll) -> ElementModQ:
     return hash_elems_with_tag(DOMAIN_TAG_SIG)
 
-def hash_elems_with_tag(tag: bytes, *a: CryptoHashableAll) -> ElementModQ:
+def hash_elems_with_tag(tag: Optional[bytes], *a: CryptoHashableAll) -> ElementModQ:
     """
     Given zero or more elements, calculate their cryptographic hash
     using SHA256 using a tag to separate domains. Allowed element types are `ElementModP`, `ElementModQ`,
     `str`, or `int`, anything implementing `CryptoHashable`, and lists
     or optionals of any of those types.
 
-    :param tag: a byte string to represent separate hash domains.
+    :param tag: an optional byte string to represent separate hash domains.
     :param a: Zero or more elements of any of the accepted types.
     :return: A cryptographic hash of these elements, concatenated.
     """
 
     # Hash tag to have fixed size (32 bytes)
-    hashed_tag = sha256(tag).digest()
-
-    # Update hash with one full size block for the tag (64 bytes)
-    h = sha256()
-    h.update(hashed_tag)
-    h.update(hashed_tag)
+    h = sha256() if tag is None else sha256(tag)
 
     h.update("|".encode(BYTE_ENCODING))
     for x in a:
