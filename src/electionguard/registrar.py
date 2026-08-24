442b4527e1032f7ed775dc2c65c83d3617c5b6a6
Jonas <jonas.kaiser@student.kit.edu>
2026-08-24 14:31:05 +0200

    Update registrar.py

--- src/electionguard/registrar.py
+++ src/electionguard/registrar.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from electionguard.eligibility_roll import EligibilityRoll
 from electionguard.group import ElementModQ, add_q, g_pow_p, rand_q
-from electionguard.schnorr_signature import SchnorrKeyPair
+from electionguard.schnorr_signature import SchnorrKeyPair, SchnorrPublicKey
 
 
 @dataclass
@@ -25,15 +25,15 @@ class Registrar:
         self.nonce = nonce
         self.credentials = {}
 
-    def publish_public_crendentials(self) -> list[SchnorrKeyPair]:
+    def publish_public_crendentials(self) -> list[SchnorrPublicKey]:
         """Publishes the public credentials for each voter in the eligibility roll."""
 
-        return [self.credentials[voter.object_id] for voter in self.eligibility_roll.voters]
+        return [self.credentials[voter.object_id].public_key for voter in self.eligibility_roll.voters]
 
     def send_credential_to_voter(self, id: str) -> SchnorrKeyPair:
         """""Sends the credential to a voter based on their ID."""
 
-        if id in self.credentials:
+        if id not in self.credentials:
             raise ValueError("Invalid voter ID")
 
         return self.credentials[id]
@@ -41,16 +41,16 @@ class Registrar:
     def generate_credentials(self):
         """Generates a Schnorr key pair for each voter in the eligibility roll."""
 
-        nonce = rand_q()
+        nonce = self.nonce if self.nonce is not None else rand_q()
 
         for (i, voter) in enumerate(self.eligibility_roll.voters):
             nonce = add_q(nonce, i)
-            credential = self.generate_credential(nonce)
+            credential = self._generate_credential(nonce)
 
             voter_id = voter.object_id
             self.credentials[voter_id] = credential
 
-    def generate_credential(self, nonce: Optional[ElementModQ] = None) -> SchnorrKeyPair:
+    def _generate_credential(self, nonce: Optional[ElementModQ] = None) -> SchnorrKeyPair:
         """Generates one Schnorr key pair"""
 
         secret_key = nonce if nonce is not None else rand_q()
